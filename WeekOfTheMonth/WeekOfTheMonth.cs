db9d3a11bc2970dc27f549005bd8f48f053d555a
Stephen Odundo <steveremdi@gmail.com>
2021-06-14 18:55:43 +0300

    Add project files.

---
+++ WeekOfTheMonth/WeekOfTheMonth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOfTheMonth
+{
+    public class WeekOfTheMonth
+    {
+        public int GetWeek(string date)
+        {
+            var isValidDate = DateTime.TryParse(date, out var dateObject);
+            if(!isValidDate)
+                return 0;
+
+            return dateObject.GetWeekOfMonth();
+        }
+    }
+
+    public static class DateTimeExtensions
+    {
+        private static GregorianCalendar _gc = new GregorianCalendar();
+        public static int GetWeekOfMonth(this DateTime time)
+        {
+            DateTime first = new DateTime(time.Year, time.Month, 1);
+            return time.GetWeekOfYear() - first.GetWeekOfYear() + 1;
+        }
+        public static int GetWeekOfYear(this DateTime time)
+        {
+            return _gc.GetWeekOfYear(time, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
+        }
+    }
+}
