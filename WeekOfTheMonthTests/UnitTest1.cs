c2a21b63a9de9ff5cef238de70f10b61792ae071
Stephen Odundo <steveremdi@gmail.com>
2021-06-14 19:52:58 +0300

    Added test methods

--- WeekOfTheMonthTests/UnitTest1.cs
+++ WeekOfTheMonthTests/UnitTest1.cs
@@ -7,8 +7,35 @@ namespace WeekOfTheMonthTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BasicDate()
         {
+            var WeekMonth = new WeekOfTheMonth.WeekOfTheMonth();
+            var week = WeekMonth.GetWeek("25/06/2021");
+            Assert.AreEqual(week, 4);
+        }
+
+        [TestMethod]
+        public void NoLeadingZeroInMonth()
+        {
+            var WeekMonth = new WeekOfTheMonth.WeekOfTheMonth();
+            var week = WeekMonth.GetWeek("25/6/2021");
+            Assert.AreEqual(week, 4);
+        }
+
+        [TestMethod]
+        public void LeapYear()
+        {
+            var WeekMonth = new WeekOfTheMonth.WeekOfTheMonth();
+            var week = WeekMonth.GetWeek("29/02/2020");
+            Assert.AreEqual(week, 5);
+        }
+
+        [TestMethod]
+        public void NonExistentDate()
+        {
+            var WeekMonth = new WeekOfTheMonth.WeekOfTheMonth();
+            var week = WeekMonth.GetWeek("29/06/2021");
+            Assert.AreEqual(week, 0);
         }
     }
 }
